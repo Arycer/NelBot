8a4f318ae5922290e4780abd45b049cba31837ee
Nellgm <NekoTsundere_Chiii@outlook.com>
2024-03-06 00:54:35 +0100

    Update pat.js

--- comandos/pat.js
+++ comandos/pat.js
@@ -1,9 +1,9 @@
 module.exports = {
-    name: 'sendimage',
+    name: 'pat',
     description: 'Envía una imagen al canal',
-    execute(message, args) {
-        message.channel.send("https://media1.tenor.com/m/aZFqg65KvssAAAAC/pat-anime.gif")
+    execute: async function (interaction) {
+        await interaction.reply("https://media1.tenor.com/m/aZFqg65KvssAAAAC/pat-anime.gif")
         .then(() => console.log('Imagen enviada correctamente'))
         .catch(error => console.error('Error al enviar la imagen:', error));
-    }
+      },
   };
