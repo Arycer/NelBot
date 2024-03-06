ce043a9639c35939a766e2c31415030f319c4ee9
Nellgm <NekoTsundere_Chiii@outlook.com>
2024-03-06 01:42:22 +0100

    Update hug.js

--- comandos/hug.js
+++ comandos/hug.js
@@ -1,18 +1,18 @@
 const randomPat = () => {
     const pats = [
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
+        "https://i.gifer.com/2QEa.gif",
+        "https://media1.tenor.com/m/J7eGDvGeP9IAAAAC/enage-kiss-anime-hug.gif",
+        "https://i.pinimg.com/originals/b6/2f/04/b62f047f8ed11b832cb6c0d8ec30687b.gif",
+        "https://gifdb.com/images/high/anime-hug-date-a-live-z8yqrk7wlwo3v8ql.gif",
+        "https://i.pinimg.com/originals/43/fc/e3/43fce3d874179afb2d9d74a7402dcff4.gif",
+        "https://i.pinimg.com/originals/4d/89/d7/4d89d7f963b41a416ec8a55230dab31b.gif",
+        "https://33.media.tumblr.com/680b69563aceba3df48b4483d007bce3/tumblr_mxre7hEX4h1sc1kfto1_500.gif",
+        "https://i.gifer.com/27tM.gif",
+        "https://gifdb.com/images/high/cute-anime-hug-gnxliwhxm4xua8l0.gif",
+        "https://i.imgur.com/IAxUnda.gif",
+        "https://i.gifer.com/B7bp.gif",
+        "https://37.media.tumblr.com/f2a878657add13aa09a5e089378ec43d/tumblr_n5uovjOi931tp7433o1_500.gif",
+        "https://i.gifer.com/Wjrj.gif",
     ];
 
     return pats[Math.floor(Math.random() * pats.length)];
