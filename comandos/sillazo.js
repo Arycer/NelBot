50bb6eb4a8e14967bf302188875cd6ebfd0f021a
Nellgm <NekoTsundere_Chiii@outlook.com>
2024-03-06 02:00:37 +0100

    Update sillazo.js

--- comandos/sillazo.js
+++ comandos/sillazo.js
@@ -4,7 +4,6 @@ const randomPat = () => {
         "https://media.tenor.com/Wr-tHDA9KtoAAAAM/miriam-pelea.gif",
         "https://media.tenor.com/dEWK-W4HvewAAAAM/evo-silletazo.gif",
         "https://i.gifer.com/SOzw.gif",
-        "https://miro.medium.com/v2/resize:fit:640/1*r8vV_cJAqP5bQTCg5t0sfg.gif",
         "https://i.imgur.com/PlfHL.gif",
         "https://pa1.aminoapps.com/6393/d7580c9a3388fd814a922818fb9abad25d72a962_hq.gif",
         "https://2img.net/h/i92.photobucket.com/albums/l31/somekindabrady/Shot.gif?t=1253716664",
