ffa9a37c225b93eeb27dc5b01c4e4e9f7940792f
Nellgm <NekoTsundere_Chiii@outlook.com>
2024-03-06 02:20:05 +0100

    nuevos comandos

---
+++ comandos/slap.js
@@ -0,0 +1,29 @@
+const randomPat = () => {
+    const pats = [
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+    ];
+
+    return pats[Math.floor(Math.random() * pats.length)];
+}
+
+module.exports = {
+    name: 'slap',
+    description: 'Envía una imagen al canal',
+    execute: async function (interaction) {
+        await interaction.reply(randomPat())
+        .then(() => console.log('Imagen enviada correctamente'))
+        .catch(error => console.error('Error al enviar la imagen:', error));
+      },
+  };
