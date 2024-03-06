6c173892c4235ba8bcbab0b44cb56734836aa133
Nellgm <NekoTsundere_Chiii@outlook.com>
2024-03-06 02:30:30 +0100

    Create cuddle.js

---
+++ comandos/cuddle.js
@@ -0,0 +1,33 @@
+const randomPat = () => {
+    const pats = [
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+        "",
+    ];
+
+    return pats[Math.floor(Math.random() * pats.length)];
+}
+
+module.exports = {
+    name: 'cuddle',
+    description: 'Envía una imagen al canal',
+    execute: async function (interaction) {
+        await interaction.reply(randomPat())
+        .then(() => console.log('Imagen enviada correctamente'))
+        .catch(error => console.error('Error al enviar la imagen:', error));
+      },
+  };
