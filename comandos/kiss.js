9bd79bba7078ed63797398f729276d6e61277a6a
Nellgm <NekoTsundere_Chiii@outlook.com>
2024-03-06 01:39:58 +0100

    Update kiss.js

--- comandos/kiss.js
+++ comandos/kiss.js
@@ -1,18 +1,18 @@
 const randomPat = () => {
     const pats = [
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
-        "",
+        "https://media1.tenor.com/m/IAP6odUutZMAAAAC/kiss.gif",
+        "https://i.pinimg.com/originals/1a/b1/b7/1ab1b7c12ccb4cb3d1002b1f7bc40903.gif",
+        "https://i.pinimg.com/originals/10/5a/7a/105a7ad7edbe74e5ca834348025cc650.gif",
+        "https://i.gifer.com/C3GK.gif",
+        "https://i.pinimg.com/originals/41/8b/d1/418bd15719e95c5b5c03e847296cb17a.gif",
+        "https://gifdb.com/images/high/anime-kissing-498-x-280-gif-op3h5wkpm21z2dil.gif",
+        "https://wethehunted.files.wordpress.com/2015/11/katanagatari-kiss.gif",
+        "https://37.media.tumblr.com/b12bd032e97037081108f993aadcae62/tumblr_mwo343m7tK1sv72vno1_500.gif",
+        "https://gifdb.com/images/high/shy-anime-kiss-sakura-trick-mfs9xkkiznvxc46a.gif",
+        "https://i.makeagif.com/media/12-24-2021/-zQ_G9.gif",
+        "https://gifdb.com/images/high/ayano-sugiura-anime-kiss-scene-zvx6dwc8h29t2hnw.gif",
+        "https://i.pinimg.com/originals/8a/e6/59/8ae65928d806391c4fa5b8cbbb14e3d5.gif",
+        "https://d31xsmoz1lk3y3.cloudfront.net/games/imgur/eisk88U.gif",
     ];
 
     return pats[Math.floor(Math.random() * pats.length)];
