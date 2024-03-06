99fcc184d71f6f2003811fe87bf9a2061f538a36
Nellgm <NekoTsundere_Chiii@outlook.com>
2024-03-06 01:13:19 +0100

    pat actualizado

--- comandos/pat.js
+++ comandos/pat.js
@@ -6,7 +6,13 @@ const randomPat = () => {
         "https://media1.tenor.com/m/mjzPtcvwO2cAAAAC/pat-head-good-job.gif",
         "https://i.pinimg.com/originals/61/51/c4/6151c42c94df654b1c7de2fdebaa6bd1.gif",
         "https://media.tenor.com/Dbg-7wAaiJwAAAAM/aharen-aharen-san.gif",
-
+        "https://i.pinimg.com/originals/2e/27/d5/2e27d5d124bc2a62ddeb5dc9e7a73dd8.gif",
+        "https://i.pinimg.com/originals/70/96/0e/70960e87fb9454df6a1d15c96c9ad955.gif",
+        "https://media.tenor.com/Dbg-7wAaiJwAAAAM/aharen-aharen-san.gif",
+        "https://tenor.com/view/pat-pat-gif-27638431",
+        "https://media1.tenor.com/m/TKJSI_X-sKIAAAAC/assassination-classroom-anime.gif",
+        "https://media1.tenor.com/m/Snem6pOBe4QAAAAC/korosensei-assassination-classroom.gif",
+        "https://i.gifer.com/3dRv.gif",
     ];
 
     return pats[Math.floor(Math.random() * pats.length)];
