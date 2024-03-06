dd7aefd415aa041f793b22faed24f1e5d09ea377
Nellgm <NekoTsundere_Chiii@outlook.com>
2024-03-06 01:30:11 +0100

    Update bite.js

--- comandos/bite.js
+++ comandos/bite.js
@@ -9,10 +9,10 @@ const randomPat = () => {
         "https://gifdb.com/images/high/anime-hickey-kiss-mark-mn1gmhqtxoa6yp3a.gif",
         "https://64.media.tumblr.com/f8e7c81e268388359d6f89da2fed0641/tumblr_nwdv3zsfoD1txbmwvo1_500.gif",
         "https://pa1.aminoapps.com/6045/a9bb6d864ebe7e01ed647b78fc652f15116716c4_hq.gif",
-        "https://qph.cf2.quoracdn.net/main-qimg-b05aeae3a266ec2003e9724d8390499c",
+        "https://i.pinimg.com/originals/c0/b4/a9/c0b4a94993a08d1df826e27e55dd2fb0.gif",
         "https://i.pinimg.com/originals/52/1d/b6/521db6745ca5cd5d094a3c01a9a674cf.gif",
-        "https://media.tenor.com/ECCpi63jZlUAAAAM/anime-bite.gif",
-        "https://media.tenor.com/1LtA9dSoAIQAAAAM/zero-no-tsukaima-bite.gif",
+        "https://pa1.aminoapps.com/6330/166b0eee3ccf1c9b1e2c9fd27f253988d940d27e_hq.gif",
+        "https://i0.wp.com/c.tenor.com/36qdN7w7I28AAAAC/anime-couple-anime-neck.gif",
     ];
 
     return pats[Math.floor(Math.random() * pats.length)];
