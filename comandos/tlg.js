a58e0fc5a11c39b55345db724ab304515a9129a8
Nellgm <NekoTsundere_Chiii@outlook.com>
2024-03-14 23:54:43 +0100

    Create tlg.js

---
+++ comandos/tlg.js
@@ -0,0 +1,10 @@
+/*
+Exportación de la función que se ejecuta al llamar al comando "ping".
+*/
+module.exports = {
+    name: "tlg",
+    description: "Nombre del grupo y significado",
+    execute: async function (interaction) {
+      await interaction.reply("TLG fue creado un dia tonto donde eramos todas inocentes pero con el paso del tiempo se convirtio en algo mas importante. TLG con el significado principal que es The Lost Generation paso a ser llamado de broma como Tanga de Leopardo Gigante para hacer la gracia, pero con el paso del tiempo se empezo llamar de otro modo donde nos identifica mejor.... su nombre es Trans+Lesbi+Gay.  "); // Responder con un mensaje al usuario.
+    },
+  };
