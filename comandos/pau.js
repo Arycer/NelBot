c515b33d17f5d4ca7b7a8e6d88e4b61064af2f06
Nellgm <NekoTsundere_Chiii@outlook.com>
2024-03-15 20:39:49 +0100

    Update pau.js

--- comandos/pau.js
+++ comandos/pau.js
@@ -1,10 +1,20 @@
+const frases = [
+  "Cuando ni ChatGPT puede ayudarte, aparecen las verdaderas diosas del Olympo a ayudarte. Gracias por todo, te orare todos los años con arroz y una tarta de fresa 🍰",
+  "1",
+  "2",
+  "3",
+]
+
 /*
 Exportación de la función que se ejecuta al llamar al comando "ping".
 */
+
 module.exports = {
     name: "pau",
     description: "Las heroinas pueden no llevar capas",
     execute: async function (interaction) {
-      await interaction.reply("<@487214130466521088> Cuando ni ChatGPT puede ayudarte, aparecen las verdaderas diosas del Olympo a ayudarte. Gracias por todo, te orare todos los años con arroz y una tarta de fresa 🍰"); // Responder con un mensaje al usuario.
+      await interaction.reply(
+        "<@487214130466521088> " + frases[Math.floor(Math.random() * frases.length)]
+        ); // Responder con un mensaje al usuario.
     },
   };
