ecd733735fa2ed88ed884033b90aaae2f24809d9
Nellgm <NekoTsundere_Chiii@outlook.com>
2024-03-21 20:12:19 +0100

    Update doritos.js

--- comandos/doritos.js
+++ comandos/doritos.js
@@ -2,7 +2,9 @@ const frases = [
     "\nDoritos siendo abstemia\nhttps://cdn.discordapp.com/attachments/1016438591309762581/1220437403433566349/IMG_20240321_192150.jpg?ex=660eefe7&is=65fc7ae7&hm=1b61394bbe614fe2b3ba0d18ef9c0e649f7fc96b1ba55e0f5d9af493ef34c1a3&",
     "Madre mia que culo",
     "\nDoritos borracha:\nhttps://cdn.discordapp.com/attachments/1068649239208546325/1220444896662393022/1711047121302.jpg?ex=660ef6e2&is=65fc81e2&hm=9b15f5b4506a384a8e5c884da752b8138ab2eceaf2ec4bb6c51c07dc7062ff67&",
-  
+    "Nel si nos conocieramos en persona de seguro nos la pasabamos bien (fan total de la saga de peliculas de SAW)",
+    
+
   ]
   
   
