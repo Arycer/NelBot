25b96b526eb560f23e4226f8049984c6045cb26c
Nellgm <162259399+Nellgm@users.noreply.github.com>
2025-01-18 08:45:13 +0100

    Update paula.js

--- comandos/paula.js
+++ comandos/paula.js
@@ -2,7 +2,7 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const { data } = require('./paula');
 
 const frases = [
-  "♡𝐿𝒶𝓈 𝓂𝒶𝓁𝒶𝓈 𝓁𝑒𝓃𝑔𝓊𝒶𝓈 𝒽𝒶𝒷𝓁𝒶𝓃, 𝓅𝑒𝓇𝑜 𝓁𝒶𝓈 𝒷𝓊𝑒𝓃𝒶𝓈 𝒹𝑒𝒿𝒶𝓃 𝓁𝒶𝓈 𝓅𝒾𝑒𝓇𝓃𝒶𝓈 𝓉𝑒𝓂𝒷𝓁𝒶𝓃𝒹𝑜.♡",
+  "♡Las malas lenguas hablan, pero las buenas te dejan las piernas temblando♡",
 
   
   
